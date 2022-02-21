f7a7d1273b1d167f9950f2d2a0a960877e561c98
t_kodama <1ec183852a@gmail.com>
2022-02-21 22:35:27 +0900

    actionsとapiのデータ周りの設定

---
+++ src/store/actions.js
@@ -0,0 +1,23 @@
+import * as types from './mutation-types'
+import { Auth, List, Task } from '../api'
+
+export default {
+  login: ({commit}) => {
+    throw new Error('login action sholud be implemented')
+  },
+  fetchLists: ({commit}) => {
+    throw new Error('fetchLists action sholud be implemented')
+  },
+  addTask: ({commit}) => {
+    throw new Error('addTask action sholud be implemented')
+  },
+  updateTask: ({commit}) => {
+    throw new Error('updateTask action sholud be implemented')
+  },
+  removeTask: ({commit}) => {
+    throw new Error('removeTask action sholud be implemented')
+  },
+  logout: ({commit}) => {
+    throw new Error('logout action should be implemented')
+  }
+}
